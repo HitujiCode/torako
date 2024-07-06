8a5991ab1135bae5da0d0a1c2d75313d6809db87
hituji_code <k.m.hituji3340@gmail.com>
2024-07-06 14:27:21 +0900

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,56 @@
+
+jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+
+  // ハンバーガーメニュー
+  $(function () {
+    $(".js-hamburger").click(function () {
+      $(this).toggleClass("is-open");
+      $(".js-drawer").toggleClass("is-open");
+      if ($(".js-drawer").hasClass("is-open")) {
+        setTimeout(function () {
+          $(".js-logo").addClass("is-open");
+        }, 500); // 0.5秒後にクラスを追加
+      } else {
+        $(".js-logo").removeClass("is-open");
+      }
+    });
+
+    // ドロワーナビのaタグをクリックで閉じる
+    $(".js-drawer a[href]").on("click", function () {
+      $(".js-hamburger").removeClass("is-open");
+      $(".js-drawer").removeClass("is-open");
+      $(".js-logo").removeClass("is-open");
+    });
+
+    // resizeイベント
+    $(window).on('resize', function () {
+      if (window.matchMedia("(min-width: 768px)").matches) {
+        $(".js-hamburger").removeClass("is-open");
+        $(".js-drawer").removeClass("is-open");
+        $(".js-logo").removeClass("is-open");
+      }
+    });
+  });
+
+  // MVスライダー
+  const mv_swiper = new Swiper('.js-mv-swiper', {
+    loop: true,
+    effect: 'fade',
+    speed: 2000,
+  });
+
+  // スライダー
+  const swiper = new Swiper('.js-slider', {
+    loop: true,
+    effect: 'fade',
+    speed: 2000,
+    slidePerView: "auto",
+    centeredSlides: true,
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+});
